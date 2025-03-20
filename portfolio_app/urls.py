74fa7006503bf2bc9aa86b2f54db9738417ec188
dondix15 <dondix03@gmail.com>
2025-03-20 13:46:50 -0600

    this works

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.portfolio, name='portfolio'),
+    #path('', views.portfolio, name='portfolio'),
+    path('', views.index, name = 'index')
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 def portfolio(request):
-    return render(request, 'portfolio_app/portfolio.html')
+    return render(request, 'portfolio_app/index.html')
+
+def index(request):
+    return HttpResponse('home page')
 # Create your views here.
